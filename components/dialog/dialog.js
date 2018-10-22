942aec3f17396025f4111cf3d87b6dcb1c240faa
Renj <itrenjunhua@163.com>
2018-10-22 15:40:39 +0800

    增加dialog组件

---
+++ components/dialog/dialog.js
@@ -0,0 +1,53 @@
+// components/dialog/dialog.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '弹框标题'
+    },
+    content: {
+      type: String,
+      value: '内容'
+    },
+    cancelText: {
+      type: String,
+      value: '取消'
+    },
+    okText: {
+      type: String,
+      value: '确定'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isShow: false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    show: function() {
+      this.setData({
+        isShow: true
+      })
+    },
+    close: function() {
+      this.setData({
+        isShow: false
+      })
+    },
+    _cancelEvent: function() {
+      this.triggerEvent('cancelEvent');
+    },
+    _okEvent: function() {
+      this.triggerEvent('okEvent');
+    },
+  }
+})
